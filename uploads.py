b04c3bb75f3d01b943986adac44228dedbda80d6
Kiran Jonnalagadda <jace@pobox.com>
2011-03-17 13:11:13 +0530

    Changed logo size and styling; new job page title

--- uploads.py
+++ uploads.py
@@ -12,12 +12,12 @@ uploaded_logos = UploadSet('logos', IMAGES)
 def configure():
     configure_uploads(app, uploaded_logos)
 
-def process_image(requestfile, maxsize=170):
+def process_image(requestfile, maxsize=(170, 130)):
     img = Image.open(requestfile)
     img.load()
-    if img.size[0] > maxsize or img.size[1] > maxsize:
-        img.thumbnail((maxsize, maxsize), Image.ANTIALIAS)
-    boximg = Image.new('RGBA', (maxsize, maxsize), (255, 255, 255, 0))
+    if img.size[0] > maxsize[0] or img.size[1] > maxsize[1]:
+        img.thumbnail(maxsize, Image.ANTIALIAS)
+    boximg = Image.new('RGBA', maxsize, (255, 255, 255, 0))
     boximg.paste(img, ((boximg.size[0]-img.size[0])/2, (boximg.size[1]-img.size[1])/2))
     savefile = StringIO()
     savefile.name = requestfile.filename
