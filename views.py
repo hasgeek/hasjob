23933b1ea28429c508c473f88f331f08b14894e6
Kiran Jonnalagadda <jace@pobox.com>
2012-06-13 19:16:14 +0530

    Search results are now sorted by date.

--- views.py
+++ views.py
@@ -502,7 +502,9 @@ def newjob():
 @app.route('/search')
 def search():
     now = datetime.utcnow()
-    results = do_search(request.args.get('q', u''), expand=True)
+    results = sorted(do_search(request.args.get('q', u''), expand=True),
+        key=lambda r: getattr(r, 'datetime', now))
+    results.reverse()
     return render_template('search.html', results=results, now=now, newlimit=newlimit)
 
 
