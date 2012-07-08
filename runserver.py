5bdc7a250af7de4562e755034f70bfbd9e7f1534
Nigel Babu <nigel@hasgeek.com>
2012-07-08 21:02:36 +0530

    rename website.py to runserver.py

--- runserver.py
+++ runserver.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
-
-from hasjob import app, models, forms, views, admin, assets, uploads, search, loghandler
+from hasjob import app, models
 from hasjob.models import db
-
-def configure():
-    log_formatter = loghandler.LocalVarFormatter()
-    file_handler = logging.FileHandler(app.config['LOGFILE'])
-    file_handler.setFormatter(log_formatter)
-    file_handler.setLevel(logging.WARNING)
-    app.logger.addHandler(file_handler)
-    if app.config['ADMINS']:
-        mail_handler = logging.handlers.SMTPHandler(app.config['MAIL_SERVER'],
-            app.config['DEFAULT_MAIL_SENDER'][1],
-            app.config['ADMINS'],
-            'hasgeek-jobs failure',
-            credentials = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']))
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
 
 
 if __name__ == '__main__':
-    configure()
 
     import sys
     # Create database table
