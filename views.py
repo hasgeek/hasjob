0f9ff881d7caad479f1c3d93f0531d9ec729fec6
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-07-11 15:20:02 +0530

    Don't generate a new CSRF token. The API has changed.

--- views.py
+++ views.py
@@ -502,9 +502,9 @@ def newjob():
     form = forms.ListingForm()
     form.job_type.choices = [(ob.id, ob.title) for ob in JobType.query.filter_by(public=True).order_by('seq')]
     form.job_category.choices = [(ob.id, ob.title) for ob in JobCategory.query.filter_by(public=True).order_by('seq')]
-    if request.method == 'POST' and request.form.get('form.id') == 'newheadline':
-        # POST request from the main page's Post a Job box.
-        form.csrf_token.data = form.generate_csrf_token(session)
+    #if request.method == 'POST' and request.form.get('form.id') == 'newheadline':
+    #    # POST request from the main page's Post a Job box.
+    #    form.csrf_token.data = form.generate_csrf_token(session)
     if request.method == 'POST' and request.form.get('form.id') != 'newheadline' and form.validate():
         # POST request from new job page, with successful validation
         # Move it to the editjob page for handling here forward
