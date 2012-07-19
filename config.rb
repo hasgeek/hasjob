b1a00e034ad48aaec70ad1746c2ae86611038565
Nigel Babu <nigel@hasgeek.com>
2012-07-19 09:58:05 +0530

    Moved sass to under static

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "hasjob/static/css"
-sass_dir = "sass"
+sass_dir = "hasjob/static/sass"
 images_dir = "hasjob/static/img"
 javascripts_dir = "hasjob/static/js"
 line_comments = false
