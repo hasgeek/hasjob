2e2b3fa9b17731fff3b3823cd383f81d4f25aaf0
Nigel Babu <nigel@hasgeek.com>
2012-07-23 00:07:17 +0530

    Add the accidentally removed search view back in

--- hasjob/views/display.py
+++ hasjob/views/display.py
@@ -202,3 +202,12 @@ def logoimage(hashid):
         # Don't show logo if post has been rejected. Could be spam
         abort(410)
     return redirect(uploaded_logos.url(post.company_logo))
+
+
+@app.route('/search')
+def search():
+    now = datetime.utcnow()
+    results = sorted(do_search(request.args.get('q', u''), expand=True),
+        key=lambda r: getattr(r, 'datetime', now))
+    results.reverse()
+    return render_template('search.html', results=results, now=now, newlimit=newlimit)
