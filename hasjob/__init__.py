3bbdd11edba8cf7ac01c5d3b32a4a7371358ef0d
Kiran Jonnalagadda <kiran@hasgeek.com>
2012-08-19 13:35:16 +0530

    oEmbed support in job listings.

--- hasjob/__init__.py
+++ hasjob/__init__.py
@@ -21,11 +21,13 @@ js = Bundle('js/libs/jquery-1.5.1.min.js',
             'js/libs/jquery.textarea-expander.js',
             'js/libs/tiny_mce/jquery.tinymce.js',
             'js/libs/jquery.form.js',
+            'js/libs/jquery.oembed.js',
             networkbar_js,
             'js/scripts.js',
             filters='jsmin', output='js/packed.js')
 
 css = Bundle(networkbar_css,
+             'css/jquery.oembed.css',
              'css/screen.css',
              filters='cssmin', output='css/packed.css')
 
