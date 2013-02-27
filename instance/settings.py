c2705314b9c0f005a8295c0ea9fd7e98448e0159
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-02-27 16:04:40 +0530

    More words to ban.

--- instance/settings.py
+++ instance/settings.py
@@ -30,13 +30,13 @@ DEFAULT_MAIL_SENDER = ('Job Board', 'test@example.com')
 MAIL_USERNAME = None
 MAIL_PASSWORD = None
 #: Logging: recipients of error emails
-ADMINS=['nigel@hasgeek.com']
+ADMINS = []
 #: Log file
-LOGFILE='error.log'
+LOGFILE = 'error.log'
 #: Use SSL for some URLs
-USE_SSL=False
+USE_SSL = False
 #: Twitter integration (register as a "client" app)
-TWITTER_ENABLED=False
+TWITTER_ENABLED = False
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
 TWITTER_ACCESS_KEY = ''
@@ -48,17 +48,18 @@ BITLY_KEY = ''
 PERIODIC_KEY = ''
 #: Throttle limit for email domain
 THROTTLE_LIMIT = 5
-SUPPORT_EMAIL = 'info@hasgeek.com'
+SUPPORT_EMAIL = 'support@hasgeek.com'
 #: Words banned in the title and their error messages
 BANNED_WORDS = [
     [['awesome'], u'We’ve had a bit too much awesome around here lately. Got another adjective?'],
     [['rockstar', 'rock star', 'rock-star'], u'You are not rich enough to hire a rockstar. Got another adjective?'],
     [['superstar', 'super star'], u'Everyone around here is a superstar. The term is redundant.'],
-    [['kickass', 'kick ass', 'kick-ass'], u'We don’t condone kicking asses around here. Got another adjective?'],
+    [['kickass', 'kick ass', 'kick-ass', 'kicka$$', 'kick-a$$', 'kick a$$'], u'We don’t condone kicking asses around here. Got another adjective?'],
     [['ninja'], u'Ninjas kill people. We can’t allow that. Got another adjective?'],
     [['urgent', 'immediate'], u'Sorry, we can’t help with urgent or immediate requirements. Geeks don’t grow on trees'],
     [['passionate'], u'Passion is implicit. Why even ask? Try another adjective?'],
     [['amazing'], u'Everybody’s amazing around here. The adjective is redundant.'],
+    [['fodu'], u'We don’t know what you mean, but that sounds like a dirty word. Got another adjective?'],
 ]
 
 #: LastUser server
