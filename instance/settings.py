63352725796d4c423ead3309ff7017081e858b85
kracekumar <me@kracekumar.com>
2013-03-10 22:52:27 +0530

    Added support to hide the button

--- instance/settings.py
+++ instance/settings.py
@@ -6,7 +6,7 @@ TYPEKIT_CODE=''
 #: Google Analytics code UA-XXXXXX-X
 GA_CODE=''
 #: Database backend
-SQLALCHEMY_DATABASE_URI = 'sqlite://'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'
 #: Secret key
 SECRET_KEY = 'make this something random'
 #: Timezone
