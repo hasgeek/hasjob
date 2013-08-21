64662667b9451630d7fdbe8a215169ef40ecef0b
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-08-21 10:58:06 +0530

    Update for Flask-WTF 0.9.0

--- hasjob/forms.py
+++ hasjob/forms.py
@@ -3,9 +3,9 @@
 import re
 import dns.resolver
 from flask import g, request
-from flask.ext.wtf import Form, TextField, TextAreaField, RadioField, FileField, BooleanField
-from flask.ext.wtf import Required, Email, Length, URL, ValidationError
-from flask.ext.wtf.html5 import EmailField
+from baseframe.forms import Form
+from wtforms import TextField, TextAreaField, RadioField, FileField, BooleanField, ValidationError, validators
+from wtforms.fields.html5 import EmailField
 from coaster import get_email_domain
 
 from .uploads import process_image, UploadNotAllowed
@@ -32,7 +32,7 @@ def optional_url(form, field):
     else:
         if ':' not in field.data:
             field.data = 'http://' + field.data
-        validator = URL(message="Invalid URL. URLs must begin with http:// or https://")
+        validator = validators.URL(message="Invalid URL. URLs must begin with http:// or https://")
         return validator(form, field)
 
 
@@ -40,18 +40,18 @@ class ListingForm(Form):
     """Form for new job posts"""
     job_headline = TextField("Headline",
         description="A single-line summary. This goes to the front page and across the network",
-        validators=[Required("A headline is required"),
-            Length(min=1, max=100, message="%(max)d characters maximum")])
-    job_type = RadioField("Type", coerce=int, validators=[Required("The job type must be specified")])
-    job_category = RadioField("Category", coerce=int, validators=[Required("Select a category")])
+        validators=[validators.Required("A headline is required"),
+            validators.Length(min=1, max=100, message="%(max)d characters maximum")])
+    job_type = RadioField("Type", coerce=int, validators=[validators.Required("The job type must be specified")])
+    job_category = RadioField("Category", coerce=int, validators=[validators.Required("Select a category")])
     job_location = TextField("Location",
         description=u'“Bangalore”, “Chennai”, “Pune”, etc or “Anywhere” (without quotes)',
-        validators=[Required(u"If this job doesn’t have a fixed location, use “Anywhere”"),
-            Length(min=3, max=80, message="%(max)d characters maximum")])
+        validators=[validators.Required(u"If this job doesn’t have a fixed location, use “Anywhere”"),
+            validators.Length(min=3, max=80, message="%(max)d characters maximum")])
     job_relocation_assist = BooleanField("Relocation assistance available")
     job_description = TextAreaField("Description",
         description=u"Our apologies for the mismatched font you see here. We’re (still) working on it",
-        validators=[Required("A description of the job is required")])
+        validators=[validators.Required("A description of the job is required")])
     job_perks = BooleanField("Job perks are available")
     job_perks_description = TextAreaField("Describe job perks",
         description=u"Stock options, free lunch, free conference passes, etc")
@@ -59,7 +59,7 @@ class ListingForm(Form):
         description=u'Example: "Send a resume to kumar@company.com". '
                     u"Don’t worry about spambots seeing your email address. "
                     u"We’ll secure it",
-        validators=[Required(u"HasGeek does not offer screening services. Please specify how candidates may apply")])
+        validators=[validators.Required(u"HasGeek does not offer screening services. Please specify how candidates may apply")])
     hr_contact = RadioField(u"Is it okay for recruiters and other "
         u"intermediaries to contact you about this listing?", coerce=boolint,
         description=u"We’ll display a notice to this effect on the listing",
@@ -70,8 +70,8 @@ class ListingForm(Form):
                     u"No intermediaries or unnamed stealth startups. Use your own real name if the company isn’t named "
                     u"yet. We do not accept listings from third parties such as recruitment consultants. Such listings "
                     u"may be removed without notice",
-        validators=[Required(u"This is required. Posting any name other than that of the actual organization is a violation of the ToS"),
-            Length(min=5, max=80, message="%(max)d characters maximum")])
+        validators=[validators.Required(u"This is required. Posting any name other than that of the actual organization is a violation of the ToS"),
+            validators.Length(min=5, max=80, message="%(max)d characters maximum")])
     company_logo = FileField("Logo",
         description=u"Optional — Your company logo will appear at the top of your listing. "
                     u"170px wide is optimal. We’ll resize automatically if it’s wider",
@@ -85,9 +85,9 @@ class ListingForm(Form):
                     u"Use your company email id: "
                     u"listings are classified by your email domain. "
                     u"Your full email address will not be revealed to applicants.",
-        validators=[Required("We need to confirm your email address before the job can be listed"),
-            Length(min=5, max=80, message="%(max)d characters maximum"),
-            Email("That does not appear to be a valid email address")])
+        validators=[validators.Required("We need to confirm your email address before the job can be listed"),
+            validators.Length(min=5, max=80, message="%(max)d characters maximum"),
+            validators.Email("That does not appear to be a valid email address")])
 
     def validate_company_logo(form, field):
         if not request.files['company_logo']:
@@ -142,21 +142,21 @@ class ListingForm(Form):
 
 class ConfirmForm(Form):
     terms_accepted = BooleanField("I accept the terms of service",
-        validators=[Required("You must accept the terms of service to publish this listing")])
+        validators=[validators.Required("You must accept the terms of service to publish this listing")])
     #promocode = TextField("Promo code")
 
 
 class WithdrawForm(Form):
     really_withdraw = BooleanField("Yes, I really want to withdraw the job listing",
-        validators=[Required(u"If you don’t want to withdraw the listing, just close this page")])
+        validators=[validators.Required(u"If you don’t want to withdraw the listing, just close this page")])
 
 
 class ReportForm(Form):
-    report_code = RadioField("Code", coerce=int, validators=[Required(u"Pick one")])
+    report_code = RadioField("Code", coerce=int, validators=[validators.Required(u"Pick one")])
 
 
 class RejectForm(Form):
-    reason = TextField('Reason', validators=[Required(u"Give a reason")])
+    reason = TextField('Reason', validators=[validators.Required(u"Give a reason")])
 
 
 class StickyForm(Form):
