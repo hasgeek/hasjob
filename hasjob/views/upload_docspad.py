34e1b60bee65db90c6c9559d47edb7b1678be95e
Rajat Khanduja <rajatkhanduja13@gmail.com>
2014-01-25 18:17:44 +0530

    Removed the view for 'upload_using_docspad'

--- hasjob/views/upload_docspad.py
+++ hasjob/views/upload_docspad.py
@@ -3,20 +3,6 @@ from hasjob import app
 from flask import render_template, request, Response, g, abort, url_for
 from hasjob import docspad
 
-@app.route("/upload_using_docspad", methods=["POST"])
-def upload():
-    if g.user:
-        uploaded_file = request.files['file']
-        try:
-            docId = docspad.upload(uploaded_file)
-            sessionId = docspad.get_session(docId)
-            return Response(status=200, mimetype='application/json',response=json.dumps({'sessionId':sessionId}))
-        except Exception as e:
-            print e.message
-            return Response(status=400, mimetype='application/json',response=json.dumps({'error':'Unable to upload file.'}))
-    else:
-        abort(401)
-
 @app.route("/view_doc/<sessionId>", methods=["GET"])
 def view_doc(sessionId):
     return render_template("view_doc.html", sessionId = sessionId)
