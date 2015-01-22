e76cade0ef80086ce406ce934531947b962ff7c8
Shreyas Satish <shreyas.satish@gmail.com>
2015-01-22 19:07:48 +0530

    fixed ordering when expanding grouped stickies

--- hasjob/static/js/app.js
+++ hasjob/static/js/app.js
@@ -4,17 +4,13 @@ $(function() {
     $('#stickie-area li.grouped').one('click', function(e){
       e.preventDefault();
       var group = $(this);
-      $.each(group.children(), function(index, node){
-        if (($(node).prop('tagName') === 'A') && index === 0) {
-          $(node).attr('href', $(node).data('href'));
-        }
-        else {
-          group.after("<li class='col-xs-12 col-md-3 col-sm-4 animated shake'>" + "<a class='stickie' href="+$(node).data('href')+" rel='bookmark'>" + $(node).html() + "</a></li>");
-          $(node).remove();
-        }
-      });
-      group.addClass('animated shake');
-      group.removeClass('grouped');
+      $($.map(group.children(), function(node){
+        return "<li class='col-xs-12 col-md-3 col-sm-4 animated shake'>" +
+                 "<a class='stickie' href="+$(node).data('href')+" rel='bookmark'>" + $(node).html() + "</a> \
+               </li>";
+        }).join(""))
+      .insertBefore(group);
+      $(group).remove();
     });
   };
   handleGroupClick();
