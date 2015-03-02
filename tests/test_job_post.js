bb150724d6eec93feaf90b5d1cf9d3e94eb397f7
Karthik Balakrishnan <karthikb351@gmail.com>
2015-03-02 13:22:39 +0530

    Increased test timeout period
    - Set step and wait timeouts to 30 seconds from the default 5s

--- tests/test_job_post.js
+++ tests/test_job_post.js
@@ -5,6 +5,9 @@ var test_username = system.env.TEST_USERNAME; // Dummy user's username that's cr
 var test_password = system.env.TEST_PASSWORD; // Dummy user's password that's create on startup
 
 
+casper.options.waitTimeout = 30000;
+casper.options.stepTimeout = 30000;
+
 // Baseframe resources not needed for now
 casper.on('resource.requested', function(requestData, request) {
   if ((requestData.url.indexOf('_baseframe') > -1)) {
