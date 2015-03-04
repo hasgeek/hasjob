c888ca35e5f210da0298058c6041cdc8d20faebd
Kiran Jonnalagadda <kiran@hasgeek.com>
2015-03-04 16:59:51 +0530

    Prompt the user to look up jobs in their location

--- hasjob/views/index.py
+++ hasjob/views/index.py
@@ -220,11 +220,22 @@ def index(basequery=None, type=None, category=None, md5sum=None, domain=None,
     elif pinsandposts:
         g.impressions = {post.id: (pinflag, post.id, is_bgroup) for pinflag, post, is_bgroup in pinsandposts}
 
+    # Test values for development:
+    if not g.user_geonameids:
+        g.user_geonameids = [1277333, 1277331, 1269750]
+    if not location and 'l' not in request.args and g.user_geonameids and (g.user or g.anon_user):
+        # No location filters? Prompt the user
+        ldata = location_geodata(g.user_geonameids)
+        location_prompts = [ldata[geonameid] for geonameid in g.user_geonameids if geonameid in ldata]
+    else:
+        location_prompts = []
+
     return render_template('index.html', pinsandposts=pinsandposts, grouped=grouped, now=now,
                            newlimit=newlimit, jobtype=type, jobcategory=category, title=title,
                            md5sum=md5sum, domain=domain, employer_name=employer_name,
-                           location=location, showall=showall, tag=tag,
+                           location=location, showall=showall, tag=tag, is_index=is_index,
                            header_campaign=header_campaign, loadmore=loadmore,
+                           location_prompts=location_prompts,
                            is_siteadmin=lastuser.has_permission('siteadmin'))
 
 
