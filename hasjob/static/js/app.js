798e7e0c09082b0ce0b63519b19c7e5bd9f8a79b
Shreyas Satish <shreyas.satish@gmail.com>
2015-03-10 12:26:40 +0530

    switching the pay widget from plain jquery to a react-inspired model-view rendering

--- hasjob/static/js/app.js
+++ hasjob/static/js/app.js
@@ -56,6 +56,47 @@ window.Hasjob.JobPost = {
   }
 };
 
+window.Hasjob.Currency = {
+  indian_rupee_encoder: function(value) {
+    value = value.toString();
+    value = value.replace(/[^0-9.]/g, '');  // Remove non-digits, assume . for decimals
+    var afterPoint = '';
+    if (value.indexOf('.') > 0)
+      afterPoint = value.substring(value.indexOf('.'), value.length);
+    value = Math.floor(value);
+    value = value.toString();
+    var lastThree = value.substring(value.length - 3);
+    var otherNumbers = value.substring(0, value.length - 3);
+    if (otherNumbers !== '')
+        lastThree = ',' + lastThree;
+    var res = '₹' + otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
+    return res;
+  },
+  prefix: function(currency){
+    var currencyMap = {
+      '': '¤',
+      'usd': '$',
+      'sgd': '$',
+      'eur': '€',
+      'gbp': '£'
+    };
+    return currencyMap[currency];
+  }
+};
+
+window.Hasjob.Util = {
+  getQueryParam: function(variable) {
+    // Source: https://css-tricks.com/snippets/javascript/get-url-variables/
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+     var pair = vars[i].split("=");
+     if(pair[0] == variable){return pair[1];}
+   }
+   return(false);
+ }
+};
+
 $(function() {
   window.Hasjob.JobPost.handleGroupClick();
   $(".pstar").off().click(window.Hasjob.JobPost.handleStarClick);
