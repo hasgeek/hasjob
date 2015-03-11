0d83c57993123c40a737d2659a73e6e10d0ed42c
Shreyas Satish <shreyas.satish@gmail.com>
2015-03-11 17:42:55 +0530

    refactoring pay slider

--- hasjob/static/js/app.js
+++ hasjob/static/js/app.js
@@ -56,33 +56,7 @@ window.Hasjob.JobPost = {
   }
 };
 
-window.Hasjob.Currency = {
-  indian_rupee_encoder: function(value) {
-    value = value.toString();
-    value = value.replace(/[^0-9.]/g, '');  // Remove non-digits, assume . for decimals
-    var afterPoint = '';
-    if (value.indexOf('.') > 0)
-      afterPoint = value.substring(value.indexOf('.'), value.length);
-    value = Math.floor(value);
-    value = value.toString();
-    var lastThree = value.substring(value.length - 3);
-    var otherNumbers = value.substring(0, value.length - 3);
-    if (otherNumbers !== '')
-        lastThree = ',' + lastThree;
-    var res = '₹' + otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree + afterPoint;
-    return res;
-  },
-  prefix: function(currency){
-    var currencyMap = {
-      '': '¤',
-      'usd': '$',
-      'sgd': '$',
-      'eur': '€',
-      'gbp': '£'
-    };
-    return currencyMap[currency];
-  }
-};
+
 
 window.Hasjob.Util = {
   getQueryParam: function(variable) {
