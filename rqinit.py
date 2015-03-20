a6e2bebc89ece2f25920e6ea76802e8dfd2c50f6
Karthik Balakrishnan <karthikb351@gmail.com>
2015-03-20 15:30:33 +0530

    Retry and Error handler
    - Retry failed job
    - Raise error on job failing

--- rqinit.py
+++ rqinit.py
@@ -2,6 +2,10 @@ from urlparse import urlparse
 
 from hasjob import init_for, app
 
+from redis import Redis
+
+import rq
+
 init_for('production')
 REDIS_URL = app.config.get('REDIS_URL', 'redis://localhost:6379/0')
 
@@ -13,3 +17,37 @@ REDIS_HOST = r.hostname
 REDIS_PORT = r.port
 REDIS_PASSWORD = r.password
 REDIS_DB = 0
+
+
+MAX_FAILURES = 3
+
+queues = None
+
+
+def error_handler(job, exc_type, exc_value, traceback):
+    job.meta.setdefault('failures', 0)
+    job.meta['failures'] += 1
+
+    # Too many failures
+    if job.meta['failures'] >= MAX_FAILURES:
+        app.logger.error('job %s: failed too many times times - moving to failed queue' % job.id)
+        job.save()
+        return True
+
+    # Requeue job and stop it from being moved into the failed queue
+    for queue in queues:
+        if queue.name == job.origin:
+            queue.enqueue_job(job)
+            return False
+
+    # Can't find queue, which should basically never happen as we only work jobs that match the given queue names and
+    # queues are transient in rq.
+    return True
+
+
+with rq.Connection():
+    queues = [rq.Queue("hasjob", connection=Redis())]
+
+    worker = rq.Worker(queues)
+    worker.push_exc_handler(error_handler)
+    worker.work()
