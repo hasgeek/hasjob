9170521097900f867030c398254383000ad3debf
Karthik Balakrishnan <karthikb351@gmail.com>
2015-07-03 12:48:02 +0530

    refactoring

--- instance/development.py
+++ instance/development.py
@@ -5,7 +5,7 @@ CACHE_REDIS_HOST = 'redis'
 #: Database backend
 SQLALCHEMY_DATABASE_URI = 'postgres://postgres:postgres@pg/postgres'
 REDIS_URL = 'redis://redis:6379/0'
-SERVER_NAME = (os.environ.get('SERVER_NAME') + ':5000') or 'hasjob.docker.dev:5000'
+SERVER_NAME = (os.environ.get('SERVER_NAME') or 'hasjob.docker.dev') + ':5000'
 #: LastUser server
 LASTUSER_SERVER = os.environ.get('LASTUSER_SERVER') or 'https://auth.hasgeek.com'
 #: LastUser client id
