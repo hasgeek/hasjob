f9dce7b04d2d8c6f874b54f3c5093e2851778e38
Kiran Jonnalagadda <kiran@hasgeek.com>
2015-11-05 13:05:57 +0530

    Deprecate shell.py in favour of manage.py shell. Fixes #249

--- manage.py
+++ manage.py
@@ -2,12 +2,16 @@
 
 from coaster.manage import init_manager
 
+import hasjob
+import hasjob.models as models
+import hasjob.forms as forms
+import hasjob.views as views
 from hasjob.models import db
 from hasjob import app, init_for
 
 
 if __name__ == '__main__':
     db.init_app(app)
-    manager = init_manager(app, db, init_for)
+    manager = init_manager(app, db, init_for, hasjob=hasjob, models=models, forms=forms, views=views)
 
     manager.run()
--- shell.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import os
-import readline
-from pprint import pprint
-
-from flask import *
-from hasjob import *
-
-os.environ['PYTHONINSPECT'] = 'True'
