1e3c326999a0c4b3ee07eb01af8eab9052b68d74
Vidya Ramakrishnan <vidya@hasgeek.in>
2015-11-25 14:18:16 +0530

    Changed to reading all filter options from server data.

--- hasjob/static/js/app.js
+++ hasjob/static/js/app.js
@@ -92,7 +92,7 @@ window.Hasjob.StickieList = {
       },
       success: function(data) {
         $('#main-content').html(data);
-        window.Hasjob.Filters.refreshFilters(window.Hasjob.FilterTypeOption2);
+        window.Hasjob.Filters.refreshFilters(window.Hasjob.CurrentJobFilterChoices2);
         NProgress.done();
       }
     });
@@ -220,37 +220,36 @@ window.Hasjob.Filters = {
     }
     return sortedFilterParams;
   },
-  refreshFilters: function(filterTypesOptions) {
-    $('.js-refresh-' + filterType).next().find('ul').css('visibility', 'visible');
+  refreshFilters: function(filterTypesChoices) {
+    $('#js-job-filters').find('ul').css('visibility', 'visible');
     $('#js-job-filters').find('li').show();
     $('#js-job-filters').find('.caret').show();
 
-    Object.keys(filterTypesOptions).forEach(function(filterType) {
+    Object.keys(filterTypesChoices).forEach(function(filterType) {
       //Flatten the array and retain only the filter names not title
       //eg:- "type": [["fulltime", "Full-time employment"], ["contract", "Short-term contract"], ["freelance", "Freelance or consulting"]] -> "type": ["fulltime", "contract", "freelance"]
-      var presentFilterOptions = filterTypesOptions[filterType].map(function(filterNameTitle) {
+      var currentFilterChoices = filterTypesChoices[filterType].map(function(filterNameTitle) {
         return filterNameTitle[0];
       });
 
-      //Create an array of all filter names, reading it from the DOM
-      var allFilterOptions = [];
-      $.each($('.js-refresh-' + filterType).next().find('input'), function(index, filterinputfield) {
-        allFilterOptions.push($(filterinputfield).val());
+      //Flatten the array and retain only the filter names not title
+      var allFilterChoices = window.Hasjob.JobFilterChoices[filterType].map(function(filterNameTitle) {
+        return filterNameTitle[0];
       });
 
-      //Filter the filter names to be hidden, by comparing with filter options received from server
-      var absentFilterOptions = allFilterOptions.filter(function(filteroption) {
-        if(presentFilterOptions.indexOf(filteroption) === -1) {
+      //Array of filter choices to be hidden
+      var absentFilterChoices = allFilterChoices.filter(function(filterChoice) {
+        if(currentFilterChoices.indexOf(filterChoice) === -1) {
           return true;
         }
       });
 
-      absentFilterOptions.forEach(function(filteroption) {
-        $('input[value="' + filteroption + '"]').parents('li').hide();
+      absentFilterChoices.forEach(function(filterChoice) {
+        $('input[value="' + filterChoice + '"]').parents('li').hide();
       });
 
-      //If all options of filter are hidden, then hide the caret to avoid empty dropdown
-      if(absentFilterOptions.length === allFilterOptions.length) {
+      //If all choices of a filter type are hidden, then hide the dropdown list
+      if(absentFilterChoices.length === allFilterChoices.length) {
         $('.js-refresh-' + filterType).next().find('ul').css('visibility', 'hidden');
         $('.js-refresh-' + filterType).next().find('.caret').hide();
       }
@@ -398,7 +397,7 @@ $(function() {
   });
 
   window.Hasjob.Filters.init();
-  window.Hasjob.Filters.refreshFilters(window.Hasjob.FilterTypeOption1);
+  window.Hasjob.Filters.refreshFilters(window.Hasjob.CurrentJobFilterChoices1);
   window.Hasjob.StickieList.init();
 
   //Change site button to filter icon
