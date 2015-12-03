bada6f3ecdb80e9bd780b374c079a14e0cf1e3c8
Shreyas Satish <shreyas.satish@gmail.com>
2015-12-03 20:52:47 +0530

    improved styling: early return for invalid case

--- hasjob/views/helper.py
+++ hasjob/views/helper.py
@@ -702,10 +702,10 @@ def filter_categories(basequery, board, filters):
 @app.context_processor
 def inject_filter_options():
     # Don't compute filter choices for paginated results
-    if not JobPost.is_paginated(request):
-        basequery = getposts(showall=True, order=False, limit=False)
-        filters = g.event_data.get('filters', {})
-        return dict(job_location_filters=filter_locations(filters),
-                    job_type_filters=filter_types(basequery, board=g.board, filters=filters),
-                    job_category_filters=filter_categories(basequery, board=g.board, filters=filters))
-    return {}
+    if JobPost.is_paginated(request):
+        return dict()
+    basequery = getposts(showall=True, order=False, limit=False)
+    filters = g.event_data.get('filters', {})
+    return dict(job_location_filters=filter_locations(filters),
+                job_type_filters=filter_types(basequery, board=g.board, filters=filters),
+                job_category_filters=filter_categories(basequery, board=g.board, filters=filters))
