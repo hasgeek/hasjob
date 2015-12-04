534a4e99fc555c0e99abf4f4ade1ac2b91a7a258
Shreyas Satish <shreyas.satish@gmail.com>
2015-12-04 21:55:50 +0530

    enable caching

--- hasjob/views/helper.py
+++ hasjob/views/helper.py
@@ -664,7 +664,7 @@ def filter_basequery(basequery, filters, exclude_list=[]):
     return basequery
 
 
-# @cache.memoize(timeout=3600)
+@cache.memoize(timeout=3600)
 def filter_locations(filters):
     now = datetime.utcnow()
     basequery = db.session.query(JobLocation.geonameid, db.func.count(JobLocation.geonameid).label('count')
@@ -681,7 +681,7 @@ def filter_locations(filters):
             for geonameid in geonameids]
 
 
-# @cache.memoize(timeout=3600)
+@cache.memoize(timeout=3600)
 def filter_types(basequery, board, filters):
     basequery = filter_basequery(basequery, filters, exclude_list=['types'])
     filtered_typeids = [post.type_id for post in basequery.options(db.load_only('type_id')).distinct('type_id').all()]
@@ -695,7 +695,7 @@ def filter_types(basequery, board, filters):
                 for job_type in JobType.query.filter_by(private=False, public=True).order_by('seq')]
 
 
-# @cache.memoize(timeout=3600)
+@cache.memoize(timeout=3600)
 def filter_categories(basequery, board, filters):
     basequery = filter_basequery(basequery, filters, exclude_list=['categories'])
     filtered_categoryids = [post.category_id for post in basequery.options(db.load_only('category_id')).distinct('category_id').all()]
