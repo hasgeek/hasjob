c04731e53b26872c97b51f4aef2f3163209c5e18
Abijith Kp <abijith.akp@gmail.com>
2016-03-06 23:35:36 +0530

    adding sweep command under manage.py - issue #221

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from coaster.manage import init_manager
+from coaster.manage import init_manager, database
 
 import hasjob
 import hasjob.models as models
@@ -8,6 +8,17 @@ import hasjob.forms as forms
 import hasjob.views as views
 from hasjob.models import db
 from hasjob import app, init_for
+from datetime import datetime, timedelta
+
+@database.option('-e', '--env', default='dev', help="runtime env [default 'dev']")
+def sweep(env):
+    """Sweep the user database to close all the inactive sessions"""
+    manager.init_for(env)
+    print "Sweeping all the inactive sessions"
+    es = models.EventSession
+    # Inactive sessions are only kept for 30mins
+    es.query.filter((es.ended_at==None) & (es.active_at<(datetime.utcnow() - \
+        timedelta(minutes=30)))).update({es.ended_at: es.active_at})
 
 
 if __name__ == '__main__':
