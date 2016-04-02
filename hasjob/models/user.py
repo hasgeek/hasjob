19e84807af11ec8669e3aa5ed3ff322bdee8a75a
Kiran Jonnalagadda <kiran@hasgeek.com>
2016-04-02 13:10:52 +0530

    Defer loading of potentially large data by default

--- hasjob/models/user.py
+++ hasjob/models/user.py
@@ -14,7 +14,7 @@ __all__ = ['User', 'UserActiveAt', 'AnonUser', 'EventSessionBase', 'EventSession
 class User(UserBase2, db.Model):
     __tablename__ = 'user'
 
-    resume = db.Column(JsonDict, nullable=False, default={})
+    resume = db.deferred(db.Column(JsonDict, nullable=False, default={}))
     blocked = db.Column(db.Boolean, nullable=False, default=False)
 
 
