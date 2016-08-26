990d16b2c96b1c7a3cdf33a0929bb8fbe07f0f0c
Shreyas Satish <shreyas.satish@gmail.com>
2016-08-26 17:30:47 +0530

    add method to make starred_job more efficient

--- hasjob/models/jobpost.py
+++ hasjob/models/jobpost.py
@@ -51,7 +51,7 @@ starred_job_table = db.Table('starred_job', db.Model.metadata,
     )
 
 
-def starred_job_ids(user, agelimit):
+def starred_job_ids(user, agelimit=None):
     if agelimit:
         return [r[0] for r in db.session.query(starred_job_table.c.jobpost_id).filter(
             starred_job_table.c.user_id == user.id,
@@ -62,6 +62,13 @@ def starred_job_ids(user, agelimit):
 
 
 User.starred_job_ids = starred_job_ids
+
+
+def has_starred_post(user, post):
+    """Checks if user has starred a particular post"""
+    return post.id in user.starred_job_ids() if post else False
+
+User.has_starred_post = has_starred_post
 
 
 class JobPost(BaseMixin, db.Model):
