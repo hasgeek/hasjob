4d2acc090122ce90dac4f4abcf4794da4b62f904
Bibhas <me@bibhas.in>
2020-02-02 14:26:54 +0530

    using lazy_ugettext again now that babel is fixed

--- hasjob/models/__init__.py
+++ hasjob/models/__init__.py
@@ -6,7 +6,7 @@ from coaster.utils import LabeledEnum
 from coaster.db import db
 from coaster.sqlalchemy import (BaseMixin, BaseNameMixin, TimestampMixin, BaseScopedIdMixin,
     BaseScopedNameMixin, CoordinatesMixin, make_timestamp_columns)
-from baseframe import _
+from baseframe import __
 from .. import app
 
 TimestampMixin.__with_timezone__ = True
@@ -16,17 +16,17 @@ newlimit = timedelta(days=1)
 
 
 class POST_STATE(LabeledEnum):
-    DRAFT =        (0, 'draft',  _("Draft"))         # Being written
-    PENDING =      (1, 'pending',  _("Pending"))       # Pending email verification
-    CONFIRMED =    (2, 'confirmed',  _("Confirmed"))     # Post is now live on site
-    REVIEWED =     (3, 'reviewed',  _("Reviewed"))      # Reviewed and cleared for push channels
-    REJECTED =     (4, 'rejected',  _("Rejected"))      # Reviewed and rejected as inappropriate
-    WITHDRAWN =    (5, 'withdrawn',  _("Withdrawn"))     # Withdrawn by owner
-    FLAGGED =      (6, 'flagged',  _("Flagged"))       # Flagged by users for review
-    SPAM =         (7, 'spam',  _("Spam"))          # Marked as spam
-    MODERATED =    (8, 'moderated',  _("Moderated"))     # Moderated, needs edit
-    ANNOUNCEMENT = (9, 'announcement',  _("Announcement"))  # Special announcement
-    CLOSED =       (10, 'closed', _("Closed"))        # Not accepting applications, but publicly viewable
+    DRAFT =        (0, 'draft',  __("Draft"))         # Being written
+    PENDING =      (1, 'pending',  __("Pending"))       # Pending email verification
+    CONFIRMED =    (2, 'confirmed',  __("Confirmed"))     # Post is now live on site
+    REVIEWED =     (3, 'reviewed',  __("Reviewed"))      # Reviewed and cleared for push channels
+    REJECTED =     (4, 'rejected',  __("Rejected"))      # Reviewed and rejected as inappropriate
+    WITHDRAWN =    (5, 'withdrawn',  __("Withdrawn"))     # Withdrawn by owner
+    FLAGGED =      (6, 'flagged',  __("Flagged"))       # Flagged by users for review
+    SPAM =         (7, 'spam',  __("Spam"))          # Marked as spam
+    MODERATED =    (8, 'moderated',  __("Moderated"))     # Moderated, needs edit
+    ANNOUNCEMENT = (9, 'announcement',  __("Announcement"))  # Special announcement
+    CLOSED =       (10, 'closed', __("Closed"))        # Not accepting applications, but publicly viewable
 
     __order__ = (DRAFT, PENDING, CONFIRMED, REVIEWED, ANNOUNCEMENT, CLOSED,
                  FLAGGED, MODERATED, REJECTED, SPAM, WITHDRAWN)
@@ -49,13 +49,13 @@ class CURRENCY(LabeledEnum):
 
 
 class EMPLOYER_RESPONSE(LabeledEnum):
-    NEW =      (0, 'new', _("New"))       # New application
-    PENDING =  (1, 'pending', _("Pending"))   # Employer viewed on website
-    IGNORED =  (2, 'ignored', _("Ignored"))   # Dismissed as not worth responding to
-    REPLIED =  (3, 'replied', _("Replied"))   # Employer replied to candidate
-    FLAGGED =  (4, 'flagged', _("Flagged"))   # Employer reported a spammer
-    SPAM =     (5, 'spam', _("Spam"))      # Admin marked this as spam
-    REJECTED = (6, 'rejected', _("Rejected"))  # Employer rejected candidate with a message
+    NEW =      (0, 'new', __("New"))       # New application
+    PENDING =  (1, 'pending', __("Pending"))   # Employer viewed on website
+    IGNORED =  (2, 'ignored', __("Ignored"))   # Dismissed as not worth responding to
+    REPLIED =  (3, 'replied', __("Replied"))   # Employer replied to candidate
+    FLAGGED =  (4, 'flagged', __("Flagged"))   # Employer reported a spammer
+    SPAM =     (5, 'spam', __("Spam"))      # Admin marked this as spam
+    REJECTED = (6, 'rejected', __("Rejected"))  # Employer rejected candidate with a message
 
     __order__ = (NEW, PENDING, IGNORED, REPLIED, FLAGGED, SPAM, REJECTED)
 
@@ -66,19 +66,19 @@ class EMPLOYER_RESPONSE(LabeledEnum):
 
 
 class PAY_TYPE(LabeledEnum):
-    NOCASH    = (0, _("Nothing"))
-    ONETIME   = (1, _("One-time"))
-    RECURRING = (2, _("Recurring"))
+    NOCASH    = (0, __("Nothing"))
+    ONETIME   = (1, __("One-time"))
+    RECURRING = (2, __("Recurring"))
 
     __order__ = (NOCASH, ONETIME, RECURRING)
 
 
 class CANDIDATE_FEEDBACK(LabeledEnum):
-    NORESPONSE =     (0, _("No response"))
-    INPROCESS =      (1, _("In process"))
-    DID_NOT_GET =    (2, _("Did not get the job"))
-    DID_NOT_ACCEPT = (3, _("Got offer, did not accept"))
-    GOT_JOB =        (4, _("Got the job"))
+    NORESPONSE =     (0, __("No response"))
+    INPROCESS =      (1, __("In process"))
+    DID_NOT_GET =    (2, __("Did not get the job"))
+    DID_NOT_ACCEPT = (3, __("Got offer, did not accept"))
+    GOT_JOB =        (4, __("Got the job"))
 
     __order__ = (NORESPONSE, INPROCESS, DID_NOT_GET, DID_NOT_ACCEPT, GOT_JOB)
 
