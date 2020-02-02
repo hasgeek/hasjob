40576acf603d7e8d5634a836346218914cc4f8b0
Bibhas <me@bibhas.in>
2020-02-02 14:38:02 +0530

    not removing the deprecated twitter functions in this PR

--- hasjob/twitter.py
+++ hasjob/twitter.py
@@ -43,3 +43,19 @@ def tweet(title, url, location=None, parsed_location=None, username=None):
     if username:
         text = text + ' @' + username
     api.update_status(text)
+
+
+# TODO: Delete this function
+def shorten(url):
+    if app.config['BITLY_KEY']:
+        b = bitlyapi.BitLy(app.config['BITLY_USER'], app.config['BITLY_KEY'])
+        res = b.shorten(longUrl=url)
+        return res['url']
+    else:
+        req = urllib2.Request("https://www.googleapis.com/urlshortener/v1/url",
+            headers={"Content-Type": "application/json"},
+            data=json.dumps({'longUrl': url}))
+        request_result = urllib2.urlopen(req)
+        result = request_result.read()
+        result_json = json.loads(result)
+        return result_json['id']
