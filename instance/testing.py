d4a3607425a933e49a6f5530d89de12688dfebd1
Bibhas <me@bibhas.in>
2020-02-05 01:41:25 +0530

    added make command to travis config

--- instance/testing.py
+++ instance/testing.py
@@ -16,7 +16,6 @@ LASTUSER_CLIENT_SECRET = os.environ.get('LASTUSER_CLIENT_SECRET', '')
 STATIC_SUBDOMAIN = 'static'
 
 ASSET_SERVER = 'https://static.hasgeek.co.in/'
-#: Server host name (and port if not 80/443)
 ASSET_MANIFEST_PATH = "static/build/manifest.json"
 # no trailing slash
 ASSET_BASE_PATH = '/static/build'
