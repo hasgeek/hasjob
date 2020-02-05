ea300ab8cd49f27adf2afa0105c446c70ffd140b
Bibhas <me@bibhas.in>
2020-02-05 09:05:02 +0530

    importing test_db in test

--- tests/integration/test_index.py
+++ tests/integration/test_index.py
@@ -4,7 +4,7 @@ from flask import url_for
 
 
 class TestIndexView:
-    def test_index(self, test_client):
+    def test_index(self, test_client, test_db):
         with test_client as c:
             resp = c.get(url_for('index', subdomain=None))
             assert "Hasjob" in resp.data.decode('utf-8')
