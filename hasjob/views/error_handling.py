c8306220a616a19aa992131372a23dd718f4f6dc
Kiran Jonnalagadda <kiran@hasgeek.com>
2022-08-13 23:23:46 +0530

    Move error templates into subfolder

--- hasjob/views/error_handling.py
+++ hasjob/views/error_handling.py
@@ -4,20 +4,20 @@ from .. import app
 
 
 @app.errorhandler(403)
-def error_403(e):
-    return render_template('403.html.jinja2'), 403
+def error_403(_exc):
+    return render_template('errors/403.html.jinja2'), 403
 
 
 @app.errorhandler(404)
-def error_404(e):
-    return render_template('404.html.jinja2'), 404
+def error_404(_exc):
+    return render_template('errors/404.html.jinja2'), 404
 
 
 @app.errorhandler(410)
-def error_410(e):
-    return render_template('410.html.jinja2'), 410
+def error_410(_exc):
+    return render_template('errors/410.html.jinja2'), 410
 
 
 @app.errorhandler(500)
-def error_500(e):
-    return render_template('500.html.jinja2'), 500
+def error_500(_exc):
+    return render_template('errors/500.html.jinja2'), 500
