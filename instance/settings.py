09af2b9a0761c69c26b9116854ddbd11d2bcf9c3
Mitesh Ashar <email@miteshashar.com>
2023-08-09 06:57:34 +0530

    Support for .env (#727)
    Co-authored-by: Kiran Jonnalagadda <kiran@hasgeek.com>

--- instance/settings.py
+++ instance/settings.py
@@ -1,69 +1,5 @@
 import re
 
-#: The title of this site
-SITE_TITLE = 'Job Board'
-#: TypeKit code for fonts
-TYPEKIT_CODE = ''
-#: Google Analytics code UA-XXXXXX-X
-GA_CODE = ''
-#: Database backend
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'
-#: Secret key
-SECRET_KEY = 'make this something random'  # nosec B105
-#: Timezone
-TIMEZONE = 'Asia/Kolkata'
-#: Static resource subdomain (defaults to 'static')
-STATIC_SUBDOMAIN = 'static'
-#: Upload path
-UPLOADED_LOGOS_DEST = '/tmp/uploads'  # nosec B108
-#: Cache settings
-CACHE_TYPE = 'redis'
-#: RQ settings
-RQ_REDIS_URL = 'redis://localhost:6379/0'
-RQ_SCHEDULER_INTERVAL = 1
-#: GeoIP database file (GeoIP2 or GeoLite2 city mmdb)
-#: On Ubuntu: /usr/share/GeoIP/GeoLite2-City.mmdb
-#: On Homebrew: /usr/local/var/GeoIP/GeoLite2-City.mmdb
-GEOIP_PATH = '/usr/share/GeoIP/GeoLite2-City.mmdb'
-#: Mail settings
-#: MAIL_FAIL_SILENTLY : default True
-#: MAIL_SERVER : default 'localhost'
-#: MAIL_PORT : default 25
-#: MAIL_USE_TLS : default False
-#: MAIL_USE_SSL : default False
-#: MAIL_USERNAME : default None
-#: MAIL_PASSWORD : default None
-#: DEFAULT_MAIL_SENDER : default None
-MAIL_FAIL_SILENTLY = False
-MAIL_SERVER = 'localhost'
-# Mail sender for crash reports and other automated mail
-DEFAULT_MAIL_SENDER = 'Hasjob <test@example.com>'
-MAIL_DEFAULT_SENDER = DEFAULT_MAIL_SENDER
-# Mail sender for job application responses (email address only)
-MAIL_SENDER = 'test@example.com'
-#: Logging: recipients of error emails
-ADMINS = []
-#: Log file
-LOGFILE = 'error.log'
-#: Use SSL for some URLs
-USE_SSL = False
-#: Twitter integration (register as a "client" app)
-TWITTER_ENABLED = False
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''  # nosec B105
-TWITTER_ACCESS_KEY = ''
-TWITTER_ACCESS_SECRET = ''  # nosec B105
-#: Bit.ly integration for short URLs
-BITLY_USER = ''
-BITLY_KEY = ''
-#: Access key for periodic server-only tasks
-PERIODIC_KEY = ''
-#: Throttle limit for email domain
-THROTTLE_LIMIT = 5
-#: Don't show year for dates within this many days
-SHORTDATE_THRESHOLD_DAYS = 60
-#: Email address to display when asking users to contact support
-SUPPORT_EMAIL = 'support@hasgeek.com'
 #: Words banned in the title and their error messages
 BANNED_WORDS = [
     [
@@ -119,9 +55,3 @@ INVALID_URLS = [
         "Candidates must apply via Hasjob",
     )
 ]
-#: LastUser server
-LASTUSER_SERVER = 'https://hasgeek.com/'
-#: LastUser client id
-LASTUSER_CLIENT_ID = ''
-#: LastUser client secret
-LASTUSER_CLIENT_SECRET = ''  # nosec B105
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,17 @@
 import os.path
 import sys
 
+from flask.cli import load_dotenv
+from flask.helpers import get_load_dotenv
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 __all__ = ['application']
 
 sys.path.insert(0, os.path.dirname(__file__))
+if get_load_dotenv():
+    load_dotenv()
+
+# pylint: disable=wrong-import-position
 from hasjob import app as application  # isort:skip
+
+application.wsgi_app = ProxyFix(application.wsgi_app)  # type: ignore[method-assign]
