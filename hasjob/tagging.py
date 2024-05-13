43aa155660bd12cfec9433ba5aca0c14d395d3fa
Kiran Jonnalagadda <kiran@hasgeek.com>
2024-05-13 23:32:25 +0530

    Move over NLTK code from Coaster; revise Ruff config

--- hasjob/tagging.py
+++ hasjob/tagging.py
@@ -1,9 +1,10 @@
 from collections import defaultdict
+from collections.abc import Iterable
 from urllib.parse import urljoin
 
+import nltk
 import requests
 
-from coaster.nlp import extract_named_entities
 from coaster.utils import text_blocks
 
 from . import app, rq
@@ -25,6 +26,35 @@ from .models import (
 
 
 @rq.job('hasjob')
+def extract_named_entities(text_blocks: Iterable[str]) -> set[str]:
+    """Return a set of named entities extracted from the provided text blocks."""
+    sentences = []
+    for text in text_blocks:
+        sentences.extend(nltk.sent_tokenize(text))
+
+    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+
+    def extract_entity_names(tree: nltk.Tree) -> list[str]:
+        entity_names = []
+
+        if hasattr(tree, "label"):
+            if tree.label() == "NE":
+                entity_names.append(" ".join(child[0] for child in tree))
+            else:
+                for child in tree:
+                    entity_names.extend(extract_entity_names(child))
+
+        return entity_names
+
+    entity_names = []
+    for tree in chunked_sentences:
+        entity_names.extend(extract_entity_names(tree))
+
+    return set(entity_names)
+
+
 def tag_locations(jobpost_id):
     with app.test_request_context():
         post = JobPost.query.get(jobpost_id)
